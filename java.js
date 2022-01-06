96eb5af1700ccd94237224f2a5769368f6c66377
pratik bhattarai <pratikbhattarai64@gmail.com>
2022-01-06 18:49:30 +0545

    sent

---
+++ java.js
@@ -0,0 +1,15 @@
+// concat method
+//1.through +
+//setting values
+let name='pratik';
+let age=14;
+let concat='I am ' +name+' I am '+age;
+
+//putting in console;
+console.log(concat)
+
+
+//through `${}`
+let another_concat=`I am ${name} and I am ${age}`
+
+console.log(another_concat);
